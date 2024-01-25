4d894308dc42f6ce625c2224be08d158f83a05e0
Diana <mada.chirica@gmail.com>
2024-01-25 12:32:43 +0000

    feat: add basic bare user controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  namespace :api, defaults: { format: :json } do
+    resources :users, only: %i[create]
+  end
 end
---
+++ test/controllers/api/users_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class Api::UsersControllerTest < ActionDispatch::IntegrationTest
+  test "returns a user id" do
+    assert true
+    assert true
+  end
+end
