74ca3ec4d2d4174a65f0821a9eb537bf933e2bfa
Diana <mada.chirica@gmail.com>
2024-02-19 16:52:29 +0000

    feat: creating a post requires authentication

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,13 +1,15 @@
 class Api::PostsController < ApplicationController
   def create
     @user = User.find(params[:author])
+
+    if @user.session_token != session[:auth_token]
+      render json: {}, status: 401
+      return
+    end
+
     @post = @user.posts.new(text: params[:text])
     if @post.save
-
       render :create
-      # render json: { 'firstName' => user.first_name, 'lastName' => user.last_name, 'userId' => user.id }, status: 200
-      # render json: { 'id' => post.id, 'text' => post.text, 'createdAt' => post.created_at,
-      #                'author' => { 'id' => user.id, 'displayName' => user.display_name } }
     else
       render :errors, status: 422
     end
--- test/controllers/api/posts_controller_test.rb
+++ test/controllers/api/posts_controller_test.rb
@@ -46,14 +46,33 @@ class Api::PostsControllerTest < ActionDispatch::IntegrationTest
     json_response = sign_in_user(user_params)
 
     user = User.find_by(id: json_response['id'])
+    user_full_name = user.first_name + ' ' + user.last_name
 
     post "/api/users/#{user.id}/posts", params: { text: post_text, author: user.id }
     assert_response :success
 
     post_response = JSON.parse(@response.body)
+    # check for text, author, createdAt
+    assert_not_nil(post_response['text'])
     assert_not_nil(post_response['author'])
-    assert_not_nil(post_response['author']['displayName'])
-    assert_equal(user.id, post_response['author']['id'])
     assert_not_nil(post_response['createdAt'])
+    # check for display name matching author's first + last name
+    assert_not_nil(post_response['author']['displayName'])
+    assert_equal(user_full_name, post_response['author']['displayName'])
+    # check for author_id being the same as the id in the URI
+    assert_equal(user.id, post_response['author']['id'])
+  end
+
+  test 'failure when creating a post without a user logged in' do
+    user = User.create!(first_name: Faker::Name.first_name,
+                        last_name: Faker::Name.last_name,
+                        password: Faker::Internet.password(min_length: 6, mix_case: true,
+                                                           special_characters: true),
+                        date_of_birth: '2000-10-20',
+                        email: Faker::Internet.email)
+
+    # assert_nil(user.id)
+    post "/api/users/#{user.id}/posts", params: { text: post_text, author: user.id }
+    assert_response 401
   end
 end
