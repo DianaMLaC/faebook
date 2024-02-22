e1b134829ae2544f51c7821b33cc95553916ac87
Diana <mada.chirica@gmail.com>
2024-02-22 14:54:58 +0000

    refactor: auth is local to posts

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,6 +1,6 @@
 class Api::PostsController < ApplicationController
   before_action :set_user_profile, only: %i[create index]
-  before_action :require_authentication
+  before_action :must_be_authorized
 
   def index
     # We need to get all the posts of the user's profile page which is not the author
@@ -14,7 +14,7 @@ class Api::PostsController < ApplicationController
 
   def create
     @post = @user.profile_posts.new(body: params[:body])
-    @post.author_id = current_user.id
+    @post.author_id = User.find_by(session_token: session[:auth_token]).id
 
     if @post.save
       render :create
@@ -27,5 +27,16 @@ class Api::PostsController < ApplicationController
 
   def set_user_profile
     @user = User.find(params[:user_id])
+  end
+
+  def must_be_authorized
+    return unless User.find_by(session_token: session[:auth_token]).nil?
+
+    # we'll redirect to log in page instead of errors.
+    render json: {
+      'errors' => {
+        'authentication' => 'Unauthorized! User need to sign in/ log in'
+      }
+    }, status: 401
   end
 end
