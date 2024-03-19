c3d594ae95f3d06dea8ac39a17797e91f516a5ea
Diana <mada.chirica@gmail.com>
2024-03-19 20:54:17 +0300

    test: unliking post twice

--- test/controllers/api/likes_controller_test.rb
+++ test/controllers/api/likes_controller_test.rb
@@ -142,4 +142,23 @@ class Api::LikesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_equal([], Like.all)
   end
+
+  test 'when a user tries to unlike a post twice then response is 404' do
+    # Arrange
+    post_author = create_and_sign_in_user(user_params)
+    post_obj = Post.create!(
+      body: faker_text,
+      author_id: post_author.id,
+      profile_id: post_author.id
+    )
+    like = Like.create!(post_id: post_obj.id,
+                        liker_id: post_author.id)
+    like.destroy
+    # Act
+    delete("/api/posts/#{post_obj.id}/likes/#{like.id}")
+    # Assert
+
+    assert_response 404
+    assert_equal([], Like.all)
+  end
 end
