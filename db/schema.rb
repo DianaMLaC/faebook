d56116468036467ed6a31efc17247d6ff17989a7
Diana <mada.chirica@gmail.com>
2024-03-19 16:31:57 +0300

    likes: g migration and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_17_105302) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_19_132916) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_17_105302) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.uuid "parent_comment_id"
+  end
+
+  create_table "likes", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "post_id"
+    t.uuid "liker_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
