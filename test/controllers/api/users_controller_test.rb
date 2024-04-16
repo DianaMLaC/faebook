741a48c54042a42b70a70f37f1501a9f0dd06ba6
Diana <mada.chirica@gmail.com>
2024-04-16 22:35:18 +0300

    test: refactored user_controller_tests

--- test/controllers/api/users_controller_test.rb
+++ test/controllers/api/users_controller_test.rb
@@ -1,166 +1,176 @@
 require 'test_helper'
 
+def createUser(user_info); end
+
 class Api::UsersControllerTest < ActionDispatch::IntegrationTest
-  def create_user(user_info, should_fail: false)
-    post '/api/users', params: user_info
+  # def create_user(user_info, should_fail: false)
+  #   post '/api/users', params: user_info
 
-    if !should_fail
-      assert_response :success
-    else
-      assert_response 422
-      return
-    end
+  #   if !should_fail
+  #     assert_response :success
+  #   else
+  #     assert_response 422
+  #     return
+  #   end
 
-    json_response = JSON.parse(@response.body)
+  #   json_response = JSON.parse(@response.body)
 
-    assert_equal(user_info[:firstName], json_response['firstName'])
-    assert_equal(user_info[:lastName], json_response['lastName'])
-    assert_nil(json_response['password'])
+  #   assert_equal(user_info[:firstName], json_response['firstName'])
+  #   assert_equal(user_info[:lastName], json_response['lastName'])
+  #   assert_nil(json_response['password'])
 
-    json_response
-  end
+  #   json_response
+  # end
 
-  def generate_user_info
-    { firstName: Faker::Name.first_name, lastName: Faker::Name.last_name,
-      password: Faker::Internet.password(min_length: 6, mix_case: true, special_characters: true), dateOfBirth: '2000-10-20', email: Faker::Internet.email }
-  end
-
-  test 'returns a user id' do
-    # ARRANGE
-
-    # ACT
-    # make a POST request to create a user
-    json_response = create_user({ firstName: 'darcy', lastName: 'smith',
-                                  password: Faker::Internet.password(min_length: 6, mix_case: true, special_characters: true),
-                                  dateOfBirth: '2000-10-20', email: Faker::Internet.email })
-
-    # Assert that the 'id' key exists
-    assert json_response.has_key?('id'), "Response should have 'id' key"
-    assert_instance_of String, json_response['id'], "'id' should be a String"
-  end
-
-  test 'user ids are different' do
-    # ARRANGE
-
-    # ACT
-    # make a POST request to create a user
-    first_user_info = generate_user_info
-    json_response = create_user(first_user_info)
-    first_user_id = json_response['id']
-
-    second_user_info = generate_user_info
-    json_response = create_user(second_user_info)
-    second_user_id = json_response['id']
-
-    assert_not_equal(first_user_id, second_user_id)
-  end
-
-  test 'user in db has same user_id' do
-    # ARRANGE
-
-    # ACT
-    # make a POST request to create a user
-    user_info = generate_user_info
-    json_response = create_user(user_info)
-    user_id = json_response['id']
-
-    user = User.find(user_id)
-    assert_not_nil(user)
-    assert_equal(user.id, user_id)
-
-    assert_equal(user.first_name, json_response['firstName'])
-    assert_equal(user.last_name, json_response['lastName'])
-  end
+  # def generate_user_info
+  #   { firstName: Faker::Name.first_name, lastName: Faker::Name.last_name,
+  #     password: Faker::Internet.password(min_length: 6, mix_case: true, special_characters: true), dateOfBirth: '2000-10-20', email: Faker::Internet.email }
+  # end
 
   # first_name and last_name
-  test 'cannot create invalid user' do
-    create_user({ firstName: '', lastName: 'Smith' }, should_fail: true)
-    create_user({ lastName: 'Smith' }, should_fail: true)
+  test 'when creating a user with no last name or first name, response is 422' do
+    post '/api/users', params:
+        { firstName: '',
+          lastName: Faker::Name.last_name,
+          password: Faker::Internet.password(min_length: 6, mix_case: true, special_characters: true),
+          dateOfBirth: '2000-10-30',
+          email: Faker::Internet.email }
+    assert_response 422
 
-    create_user({ firstName: 'Jane', lastName: '' }, should_fail: true)
-    create_user({ firstName: 'Jane' }, should_fail: true)
-
-    create_user({}, should_fail: true)
+    post '/api/users', params:
+        { firstName: Faker::Name.first_name,
+          lastName: '',
+          password: Faker::Internet.password(min_length: 6, mix_case: true, special_characters: true),
+          dateOfBirth: '2000-10-30',
+          email: Faker::Internet.email }
+    assert_response 422
   end
 
   # password
-  test 'password must be given' do
-    post '/api/users', params: { firstName: 'Jane', lastName: 'Smith' }
+  test 'when creating a user with no password, response is 422' do
+    post '/api/users', params:
+        { firstName: Faker::Name.first_name,
+          lastName: Faker::Name.last_name,
+          dateOfBirth: '2000-10-30',
+          email: Faker::Internet.email }
     assert_response 422
   end
 
-  test 'password should be minimum 6 characters and mix case' do
-    post '/api/users', params: { firstName: 'Jane', lastName: 'Smith', password: 'cats' }
+  test 'when creating a user with invalid password, response is 422' do
+    # less than 6 chars
+    post '/api/users', params:
+        { firstName: Faker::Name.first_name,
+          lastName: Faker::Name.last_name,
+          password: 'Cats',
+          dateOfBirth: '2000-10-30',
+          email: Faker::Internet.email }
     assert_response 422
 
-    post '/api/users', params: { firstName: 'Jane', lastName: 'Smith', password: 'password' }
+    # not mixed cased
+    post '/api/users', params:
+        { firstName: Faker::Name.first_name,
+          lastName: Faker::Name.last_name,
+          password: 'password',
+          dateOfBirth: '2000-10-30',
+          email: Faker::Internet.email }
     assert_response 422
-
-    # post "/api/users", params: { firstName: 'Jane', lastName: 'Smith', password: 'Password'}
-    # assert_response :success
   end
 
-  test 'password belonging to user can be retrieved' do
-    post '/api/users',
-         params: { firstName: 'Julie', lastName: 'Soul', password: 'PassworD', dateOfBirth: '2000-10-30',
-                   email: 'jane@smith.com' }
-    assert_response 200
+  test 'when trying to retrieve a users password, response is 200' do
+    # Arrange
+    password = Faker::Internet.password(min_length: 6, mix_case: true, special_characters: true)
+    post '/api/users', params:
+        { firstName: Faker::Name.first_name,
+          lastName: Faker::Name.last_name,
+          password:,
+          dateOfBirth: '2000-10-30',
+          email: Faker::Internet.email }
 
+    assert_response :success
     json_response = JSON.parse(@response.body)
+
+    # Act
     user = User.find_by(id: json_response['id'])
+
+    # Assert
     assert_not_nil(user.password_digest)
-    assert(user.has_password?('PassworD'))
+    assert(user.has_password?(password))
   end
 
   # DOB
-  test 'dob must be given' do
-    post '/api/users', params: { firstName: 'Jane', lastName: 'Smith', password: 'Password' }
+  test 'when creating a user with no dob, response is 422' do
+    post '/api/users', params:
+        { firstName: Faker::Name.first_name,
+          lastName: Faker::Name.last_name,
+          password: Faker::Internet.password(min_length: 6, mix_case: true, special_characters: true),
+          email: Faker::Internet.email }
     assert_response 422
-
-    # post "/api/users", params: { firstName: 'Jane', lastName: 'Smith', password: 'Password', dateOfBirth: '2000-10-20'}
-    # assert_response 200
   end
 
-  test 'user must be at least 14 years old' do
-    post '/api/users', params: { firstName: 'Jane', lastName: 'Smith', password: 'Password', dateOfBirth: '2015-10-20' }
+  test 'when creating a user with with dob less than 14 years ago, response is 422' do
+    post '/api/users', params:
+        { firstName: Faker::Name.first_name,
+          lastName: Faker::Name.last_name,
+          password: Faker::Internet.password(min_length: 6, mix_case: true, special_characters: true),
+          dateOfBirth: '2015-10-03',
+          email: Faker::Internet.email }
     assert_response 422
   end
 
   # Email
-  test 'email must be given' do
-    post '/api/users', params: { firstName: 'Jane', lastName: 'Smith', password: 'Password', dateOfBirth: '2000-10-20' }
-    assert_response 422
-
-    post '/api/users',
-         params: { firstName: 'Jane', lastName: 'Smith', password: 'Password', dateOfBirth: '2000-10-20',
-                   email: 'jane@smith.com' }
-    assert_response 200
-  end
-
-  test "email must have the correct format with '@' and '.' after" do
-    post '/api/users',
-         params: { firstName: 'Jane', lastName: 'Smith', password: 'Password', dateOfBirth: '2000-10-20',
-                   email: 'janesmith' }
-    assert_response 422
-
-    post '/api/users',
-         params: { firstName: 'Jane', lastName: 'Smith', password: 'Password', dateOfBirth: '2000-10-20',
-                   email: 'jane@smith' }
-    assert_response 422
-
-    post '/api/users',
-         params: { firstName: 'Jane', lastName: 'Smith', password: 'Password', dateOfBirth: '2000-10-20',
-                   email: 'ja.ne@smith' }
+  test 'when creating a user with no email, response is 422' do
+    post '/api/users', params:
+        { firstName: Faker::Name.first_name,
+          lastName: Faker::Name.last_name,
+          password: Faker::Internet.password(min_length: 6, mix_case: true, special_characters: true),
+          dateOfBirth: '2000-10-30' }
     assert_response 422
   end
 
-  test 'email must be unique' do
-    post '/api/users',
-         params: { firstName: 'Jane', lastName: 'Smith', password: 'Password', dateOfBirth: '2000-10-20',
-                   email: 'jane@smith.com' }
-    post '/api/users',
-         params: { firstName: 'Julie', lastName: 'Soul', password: 'PassworD', dateOfBirth: '2000-10-30',
-                   email: 'jane@smith.com' }
+  test 'when creating a user with an email with wrong format, response is 422' do
+    post '/api/users', params:
+        { firstName: Faker::Name.first_name,
+          lastName: Faker::Name.last_name,
+          password: Faker::Internet.password(min_length: 6, mix_case: true, special_characters: true),
+          dateOfBirth: '2000-10-03',
+          email: 'janesmith' }
+    assert_response 422
+
+    post '/api/users', params:
+        { firstName: Faker::Name.first_name,
+          lastName: Faker::Name.last_name,
+          password: Faker::Internet.password(min_length: 6, mix_case: true, special_characters: true),
+          dateOfBirth: '2000-10-03',
+          email: 'jane@smith' }
+    assert_response 422
+
+    post '/api/users', params:
+        { firstName: Faker::Name.first_name,
+          lastName: Faker::Name.last_name,
+          password: Faker::Internet.password(min_length: 6, mix_case: true, special_characters: true),
+          dateOfBirth: '2000-10-03',
+          email: 'ja.ne@smith' }
+    assert_response 422
+  end
+
+  test 'when creating a user with an email already in db, response is 422' do
+    # Arrange
+    post '/api/users', params:
+        { firstName: Faker::Name.first_name,
+          lastName: Faker::Name.last_name,
+          password: Faker::Internet.password(min_length: 6, mix_case: true, special_characters: true),
+          dateOfBirth: '2000-10-03',
+          email: 'jane@smith.com' }
+
+    # Act
+    post '/api/users', params:
+        { firstName: Faker::Name.first_name,
+          lastName: Faker::Name.last_name,
+          password: Faker::Internet.password(min_length: 6, mix_case: true, special_characters: true),
+          dateOfBirth: '2000-10-03',
+          email: 'jane@smith.com' }
+
+    # Assert
 
     assert_response 422
 
@@ -178,13 +188,41 @@ class Api::UsersControllerTest < ActionDispatch::IntegrationTest
   # Session
 
   test 'sets session aka signs user in upon creation' do
-    user_info = generate_user_info
-    json_response = create_user(user_info)
+    # Arrange
+    post '/api/users', params:
+        { firstName: Faker::Name.first_name,
+          lastName: Faker::Name.last_name,
+          password: Faker::Internet.password(min_length: 6, mix_case: true, special_characters: true),
+          dateOfBirth: '2000-10-03',
+          email: Faker::Internet.email }
 
+    # Act
+    resp = JSON.parse(@response.body)
+
+    # Assert
     assert_not_nil(session[:auth_token])
 
-    user_id = json_response['id']
-    user = User.find(user_id)
+    user_id = resp['id']
+    user = User.find_by(id: user_id)
     assert_equal(session[:auth_token], user.session_token)
+  end
+
+  test 'when fetching a user profile, response is 200' do
+    # Arrange
+    post '/api/users', params:
+        { firstName: Faker::Name.first_name,
+          lastName: Faker::Name.last_name,
+          password: Faker::Internet.password(min_length: 6, mix_case: true, special_characters: true),
+          dateOfBirth: '2000-10-03',
+          email: Faker::Internet.email }
+
+    resp = JSON.parse(@response.body)
+    user_id = resp['id']
+
+    # Act
+    get "/api/users/#{user_id}"
+
+    # Assert
+    assert_response :success
   end
 end
